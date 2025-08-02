552a4789b45159e13e8ed38b3a7b883ad4db44b4
braydenmcherry <braydenmcherry@gmail.com>
2025-08-02 23:02:58 +0000

    settings page

--- src/app/(app)/settings/page.tsx
+++ src/app/(app)/settings/page.tsx
@@ -1,11 +1,13 @@
 // src/app/(app)/settings/page.tsx
 import React from 'react';
+import { ConnectBrokerageButton } from '@/components/connect-brokerage-button';
 
 const SettingsPage = () => {
   return (
     <div>
       <h1>Settings Page</h1>
       {/* Add your settings content here */}
+ <ConnectBrokerageButton />
     </div>
   );
 };
